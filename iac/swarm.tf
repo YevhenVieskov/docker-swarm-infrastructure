ecc4b1c7eb960450210ebdd1d3ea790f17ea08fb
YevhenVieskov <evveskov@gmail.com>
2023-05-28 17:24:05 +0300

    simple infrastructure created

---
+++ iac/swarm.tf
@@ -0,0 +1,113 @@
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "${var.region}a"
+
+  tags = {
+    Name = "Default subnet for us-west-2a"
+  }
+}
+
+resource "aws_instance" "swarm-manager" {
+  count = "${var.swarm_managers}"
+  ami = "${var.swarm_ami_id}"
+  instance_type = "${var.swarm_instance_type}"
+  subnet_id     = aws_default_subnet.default_az1.id
+  #user_data                   = "${file("bootstrap.sh")}"
+  tags = {
+    Name = "swarm-manager"
+  }
+  vpc_security_group_ids = [
+    "${aws_security_group.docker.id}"
+  ]
+  key_name = var.private_key_name
+
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = "${file(var.private_key_path)}"
+    host        = aws_instance.swarm-manager[count.index].public_ip
+  }
+
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/home/ubuntu/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/ubuntu/bootstrap.sh",
+      "sh /home/ubuntu/bootstrap.sh"      
+    ]
+  }
+
+  
+  provisioner "remote-exec" {
+    inline = [      
+      "docker swarm init", 
+      "docker swarm join-token --quiet worker > /home/ubuntu/token"
+    ]
+      
+  }
+  
+}
+
+resource "aws_instance" "swarm-worker" {
+  count = "${var.swarm_workers}"
+  ami = "${var.swarm_ami_id}"
+  instance_type = "${var.swarm_instance_type}"
+  subnet_id     = aws_default_subnet.default_az1.id
+  #user_data                   = "${file("bootstrap_bash.sh")}"
+  tags = {
+    Name = "swarm-worker"
+  }
+  vpc_security_group_ids = [
+    "${aws_security_group.docker.id}"
+  ]
+  key_name = var.private_key_name
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = "${file(var.private_key_path)}"
+    host        = aws_instance.swarm-worker[count.index].public_ip
+  }
+ 
+   provisioner "file" {
+    source = var.private_key_path
+    destination = "/home/ubuntu/attract_key.pem"
+  }
+
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/home/ubuntu/bootstrap.sh"
+  }
+
+  
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/ubuntu/bootstrap.sh",
+      "sh /home/ubuntu/bootstrap.sh"      
+    ]
+  }
+
+  provisioner "remote-exec" {
+    inline = [      
+      "sudo chmod 400 /home/ubuntu/attract_key.pem",
+      "sudo scp -o StrictHostKeyChecking=no -o NoHostAuthenticationForLocalhost=yes -o UserKnownHostsFile=/dev/null -i attract_key.pem ubuntu@${aws_instance.swarm-manager[count.index].private_ip}:/home/ubuntu/token .",
+      "sudo docker swarm join --token $(cat /home/ubuntu/token) ${aws_instance.swarm-manager[count.index].private_ip}:2377"
+    ]
+  }
+  
+}
+
+resource "aws_eip" "swarm-manager-eip" {
+  instance = aws_instance.swarm-manager.0.id
+  domain   = "vpc"
+}
+
+resource "aws_eip_association" "swarm-manager-association" {
+  instance_id   = aws_instance.swarm-manager.0.id
+  allocation_id = aws_eip.swarm-manager-eip.id
+}
+
+
+
+
---
+++ iac/variables.tf
@@ -0,0 +1,60 @@
+variable "swarm_manager_token" {
+  default = ""
+}
+variable "swarm_worker_token" {
+  default = ""
+}
+variable "swarm_ami_id" {
+  default = "ami-03f65b8614a860c29"
+}
+variable "swarm_manager_ip" {
+  default = ""
+}
+variable "swarm_managers" {
+  default = 1
+}
+variable "swarm_workers" {
+  default = 1
+}
+variable "swarm_instance_type" {
+  default = "t2.micro"
+}
+variable "swarm_init" {
+  default = false
+}
+
+variable "profile" {
+  description = "AWS Profile"
+  type        = string
+  default     = "vieskovtf"
+}
+
+variable "region" {
+  description = "Region for AWS resources"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "private_key_name" {
+  description = "The name of an EC2 Key Pair that can be used to SSH to the EC2 Instances in this cluster. Set to an empty string to not associate a Key Pair."
+  type        = string
+  default     = "attract_key"
+}
+
+variable "udata_path" {
+  description = "Name to be used for the Jenkins master instance"
+  type        = string
+  default     = "./bootsrap.sh"
+}
+
+variable "private_key_path" {
+  description = "The path to an EC2 Key Pair"
+  type        = string
+  default     = "/home/yevhenv/attract_group/attract_key.pem"
+}
+
+variable "dest_private_key_path" {
+  description = "The path to an EC2 Key Pair"
+  type        = string
+  default     = "/home/ubuntu/attract_key.pem"
+}
